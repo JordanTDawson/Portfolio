9ac883564a173a4fb7b80610942ce68e6dd6002c
JordanTDawson <jtdawson@icloud.com>
2023-08-28 20:56:57 +0000

    Added self prtrait of me to the intro ection of the website.

--- portfolio/src/app/intro/page.tsx
+++ portfolio/src/app/intro/page.tsx
@@ -1,15 +1,26 @@
+import Image from "next/image";
+
 export default function Intro() {
     return (
-        <div className='max-w-[100%] md:max-w-[80%] lg:max-w-[60%] text-center'>
+        <div className='flex flex-col max-w-[100%] md:max-w-[80%] lg:max-w-[60%] text-center items-center'>
           <p className='text-gray-500'>Hi! My name is</p>
+          <div className='relative w-[200px] h-[200px] 375:w-[125px] 375:h-[125px] md:w-[400px] md:h-[400px] m-2 overflow-hidden rounded-full'>
+              <Image
+              className='object-cover w-[100%] h-[100%]'
+              src='/jordan-dawson.jpg'
+              alt='A picture of me!'
+              fill
+              priority
+              />
+          </div>
           <h1 className='text-5xl md:text-8xl'>Jordan Dawson</h1>
-          <p className='text-gray-500'> 
+          <p className='text-md text-gray-500'> 
               a driven and enthusiastic software engineer who has recently completed a comprehensive coding bootcamp.
               I am thrilled to embark on this exciting journey of transforming my passion for technology into practical skills and real-world solutions.
           </p>
           <div className='flex flex-wrap gap-8 justify-center my-4'>
-            <a href='https://www.linkedin.com/in/jordantdawson/' rel='noreferer' target='_blank' className='border-2 border-double border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform'>LinkedIn</a>
-            <a href='https://github.com/JordanTDawson' rel='noreferer' target='_blank' className='border-2 border-double border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform'>Github</a>
+            <a href='https://www.linkedin.com/in/jordantdawson/' rel='noreferer' target='_blank' className='border-2 border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform'>LinkedIn</a>
+            <a href='https://github.com/JordanTDawson' rel='noreferer' target='_blank' className='border-2 border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform'>Github</a>
           </div>
         </div>
     );
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -8,47 +8,47 @@ export default function Home() {
     <div className='snap-y snap-mandatory md:ml-[150px] md:p-4'>
       <section className='relative flex flex-col justify-center items-center min-h-screen'>
         <Intro />
-        <div className='block w-[100%] md:hidden flex justify-center 375:relative absolute bottom-0 left-0 right-0 p-4'>
-          <a className='border-2 border-double border-white rounded-lg text-xl p-4 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#skills'>
+        <div className='block w-[100%] md:hidden flex justify-center 375:relative  bottom-0 left-0 right-0 p-4'>
+          <a className='border-2 border-white rounded-lg text-xl p-4 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#skills'>
             Go to Skills
           </a>
         </div>
       </section>
       <section className='relative flex flex-col justify-center items-center min-h-screen' id='skills'>
       <div className='block w-[100%] md:hidden flex justify-center relative top-0 left-0 right-0 my-2'>
-          <a className='border-2 border-double border-white rounded-lg text-xl p-4 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#'>
+          <a className='border-2 border-white rounded-lg text-xl p-4 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#'>
             Go to Intro
           </a>
         </div>
         <Skills />
         <div className='block w-[100%] md:hidden flex justify-center relative bottom-0 left-0 right-0 my-2'>
-          <a className='border-2 border-double border-white rounded-lg text-xl p-4 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#projects'>
+          <a className='border-2 border-white rounded-lg text-xl p-4 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#projects'>
             Go to Projects
           </a>
         </div>
       </section>
       <section className='relative flex flex-col justify-center items-center min-h-screen' id='projects'>
       <div className='block w-[100%] md:hidden flex justify-center relative top-0 left-0 right-0 p-2'>
-          <a className='border-2 border-double border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#skills'>
+          <a className='border-2 border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#skills'>
             Go to Skills
           </a>
         </div>
         <Projects />
         <div className='block w-[100%] md:hidden flex justify-center relative bottom-0 left-0 right-0 p-2'>
-          <a className='border-2 border-double border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#credentials'>
+          <a className='border-2 border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#credentials'>
             Go to Credentials
           </a>
         </div>
       </section>
       <section className='relative flex flex-col justify-center items-center min-h-screen' id='credentials'>
         <div className='block w-[100%] md:hidden flex justify-center relative top-0 left-0 right-0 p-2'>
-          <a className='border-2 border-double border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#projects'>
+          <a className='border-2 border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#projects'>
             Go to Projects
           </a>
         </div>
         <Credentials />
         <div className='block w-[100%] md:hidden flex justify-center relative bottom-0 left-0 right-0 p-2'>
-          <a className='border-2 border-double border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#'>
+          <a className='border-2 border-white rounded-lg text-xl p-2 hover:text-yellow-500 hover:scale-105 transform transition-transform' href='#'>
             Go to Top
           </a>
         </div>
